ebc38b552eecf6d7fc1a4a9de04b6055398f16a6
Steven Kalinke <steven.kalinke@eccenca.com>
2019-01-06 19:02:49 +0100

    Fix SW syntax

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,11 +1,7 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-  .register('/sw.js', /* {scope: './'} */)
-  .then(() => {
-    console.log("Service Worker registered!");
-  }).catch((error) => {
-    document.querySelector('#status').textContent = error;
-  });
+  navigator.serviceWorker.register('/sw.js')
+    .then(reg => console.log('SW registered!', reg))
+    .catch(err => console.log('Boo!', err));
 } else {
   // Service Worker are not supported
   console.log("Service Workers are not supported");
