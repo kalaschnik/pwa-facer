a9f0f1947bf917f37b6e95f4764255046ca42eb2
Steven Kalinke <steven.kalinke@eccenca.com>
2019-01-08 22:28:53 +0100

    Add beforeinstallprompt to catch the Add to Homescreen Event

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,8 +1,23 @@
+// make a global variable, since we want to use it in feed.js
+var deferredPrompt;
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js')
-    .then(reg => console.log('SW registered!', reg))
+    .then(reg => console.log('SW registered!!', reg))
     .catch(err => console.log('Boo!', err));
 } else {
   // Service Worker are not supported
   console.log("Service Workers are not supported");
 }
+
+// catch the add to homescreen prompt and re-define it
+window.addEventListener("beforeinstallprompt", bannerevent => {
+
+  // the banner simply wont show with this:
+  console.log("beforeinstallprompt fired");
+  bannerevent.preventDefault();
+
+  deferredPrompt = bannerevent;
+  return false;
+
+});
