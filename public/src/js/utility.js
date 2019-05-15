f14613816c6b2a9d746568b480bb72ba0f4de5bd
Steven Kalinke <steven.kalinke@outlook.com>
2019-05-15 18:53:06 +0200

    Add deleteItemFromData helper function

--- public/src/js/utility.js
+++ public/src/js/utility.js
@@ -1,3 +1,5 @@
+import { log } from "util";
+
 // idb.open(name, version, upgradeCallback)
 // eslint-disable-next-line no-var
 var dbPromise = idb.open('faces-store', 1, (upgradeDB) => {
@@ -32,5 +34,18 @@ function clearData(st) {
       const store = tx.objectStore(st);
       store.clear();
       return tx.complete;
+    });
+}
+
+function deleteItemFromData(st, id) {
+  return dbPromise
+    .then((db) => {
+      const tx = db.transaction(st, 'readwrite');
+      const store = tx.objectStore(st);
+      store.delete(id);
+      return tx.complete;
+    })
+    .then(() => {
+      console.log('Item deleted');
     });
 }
